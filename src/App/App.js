d245ce0c60f6690aeee55c555185cafa989cd752
gunhwalee <ghlee2588@gmail.com>
2023-03-14 15:26:11 +0900

    build: 초기 설정

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Reactree</h1>
+      </header>
+      <main>
+        <div>렌더링 화면 구역</div>
+        <div>트리 구조 구역</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
