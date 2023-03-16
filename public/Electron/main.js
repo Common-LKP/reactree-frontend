1ac4e399657e57683c17d43996b843fdf04c6c6c
gunhwalee <ghlee2588@gmail.com>
2023-03-17 02:15:25 +0900

    feat: 렌더링 에러처리(포트번호 처리)

--- public/Electron/main.js
+++ public/Electron/main.js
@@ -6,9 +6,37 @@ const {
   BrowserView,
   dialog,
 } = require("electron");
-const { exec } = require("child_process");
+const { exec, spawnSync, execSync } = require("child_process");
 const path = require("path");
+const os = require("os");
 const waitOn = require("wait-on");
+
+const checkPortNumber = async () => {
+  let defaultPort = 3000;
+  const stdout = execSync(
+    "netstat -anv | grep LISTEN | awk '{print $4}'",
+  ).toString();
+
+  console.log(stdout);
+  const lines = stdout.split(os.EOL);
+  if (lines[lines.length - 1] === "") {
+    lines.pop();
+  }
+
+  for (let i = 0; i < lines.length; i += 1) {
+    lines[i] = lines[i].slice(lines[i].indexOf(".") + 1);
+  }
+
+  const portArray = lines
+    .filter(port => port > 999 && port < 10000)
+    .map(Number);
+  portArray.sort((a, b) => a - b);
+  portArray.forEach(port => {
+    if (port === defaultPort) defaultPort += 1;
+  });
+  console.log(`http://localhost:${defaultPort}`);
+  return defaultPort;
+};
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -51,34 +79,62 @@ ipcMain.handle("get-path", async () => {
     const { canceled, filePaths } = await dialog.showOpenDialog({
       properties: ["openDirectory"],
     });
+    const view = new BrowserView();
+
+    BrowserWindow.getFocusedWindow().setBrowserView(view);
+    view.setBounds({
+      x: 50,
+      y: 220,
+      width: 700,
+      height: 600,
+    });
+    view.setBackgroundColor("#ffffff");
+    view.webContents.loadFile(path.join(__dirname, "../views/loading.html"));
+    const portNumber = await checkPortNumber();
 
     if (!canceled && filePaths.length > 0) {
       const projectPath = filePaths[0];
-      exec("BROWSER=none npm run start", {
+
+      BrowserWindow.getFocusedWindow().setBrowserView(view);
+      view.setBounds({
+        x: 50,
+        y: 220,
+        width: 700,
+        height: 600,
+      });
+      view.setBackgroundColor("#ffffff");
+      view.webContents.loadFile(path.join(__dirname, "../views/loading.html"));
+      console.log("a", portNumber);
+
+      exec(`PORT=${portNumber} BROWSER=none npm run start`, {
         cwd: projectPath,
       });
+      console.log("b", portNumber);
 
-      await waitOn({ resources: ["http://localhost:3000"] });
-
-      return "http://localhost:3000";
+      await waitOn({ resources: [`http://localhost:${portNumber}`] });
+      console.log("c", portNumber);
+      view.webContents.loadURL(`http://localhost:${portNumber}`);
     }
     return null;
   } catch (error) {
+    console.log(error.message);
     throw new Error("유효하지않은 url입니다.");
   }
 });
 
-ipcMain.on("load-url", (event, url) => {
-  const win = BrowserWindow.getFocusedWindow();
-  const view = new BrowserView();
+// ipcMain.on("load-url", (event, url) => {
+//   console.log("url : ", url);
+//   const win = BrowserWindow.getFocusedWindow();
+//   const view = new BrowserView();
 
-  win.setBrowserView(view);
-  view.setBounds({
-    x: 50,
-    y: 220,
-    width: 700,
-    height: 600,
-  });
+//   win.setBrowserView(view);
+//   view.setBounds({
+//     x: 50,
+//     y: 220,
+//     width: 700,
+//     height: 600,
+//   });
+//   view.setBackgroundColor("#ffffff");
 
-  view.webContents.loadURL(url);
-});
+//   view.webContents.loadURL(url);
+// });
