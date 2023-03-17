bb2ac50ab16bec17fef1c6384faee98e8e1d6940
Minju <50537876+pmjuu@users.noreply.github.com>
2023-03-17 11:14:26 +0900

    Delete preload.js

--- public/Electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-window.ipcRenderer = require("electron").ipcRenderer;
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  openFileDialog: () => ipcRenderer.invoke("get-path"),
-  treeData: () => ipcRenderer.invoke("treeData").then(result => result),
-});
