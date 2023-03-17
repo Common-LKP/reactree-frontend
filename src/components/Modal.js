8c9aad080636c6b18b363bdbb945b9599d1a16ea
pmjuu <jsi04049@gmail.com>
2023-03-17 12:27:08 +0900

    feat: mock데이터 기반 트리구조 구현

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable prettier/prettier */
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: #ffffff;
+`;
+
+export default function Modal() {
+  return <Wrapper>modal</Wrapper>;
+}
