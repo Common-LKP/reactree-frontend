cdebc3e0317d4ca22f7ebc7c107f8fd58384e3e4
pmjuu <jsi04049@gmail.com>
2023-03-23 20:23:30 +0900

    test: deepCopy 테스트 코드 작성

--- __tests__/unit/react/util.test.js
+++ __tests__/unit/react/util.test.js
@@ -1,19 +1,49 @@
+import deepCopy from "../../../src/utils/deepCopy";
+import getTreeSVG from "../../../src/utils/getTreeSVG";
+
 describe("deeCopy", () => {
-  it("test sample", () => {
-    expect(1).toBe(1);
+  it("null을 입력하면 빈 객체를 반환합니다.", () => {
+    const result = deepCopy(null);
+
+    expect(typeof result).toBe("object");
+    expect(result).toEqual({});
   });
 
-  it("test sample2", () => {
-    expect(1).toBe(1);
+  it("child 속성이 있는 객체를 입력하면 child 속성값이 깊은 복사된 객체를 반환합니다.", () => {
+    const mockData = {
+      tag: 1,
+      child: {
+        tag: 2,
+        child: {
+          tag: 3,
+        },
+      },
+    };
+    const result = deepCopy(mockData);
+    mockData.child.tag = "new value";
+
+    expect(result).not.toEqual(mockData);
+  });
+
+  it("child 속성이 없는 객체를 입력하면 얕은 복사된 객체를 반환합니다.", () => {
+    const mockData = {
+      tag: 1,
+      notChild: {
+        tag: 2,
+        notChild: {
+          tag: 3,
+        },
+      },
+    };
+    const result = deepCopy(mockData);
+    mockData.notChild.tag = "new value";
+
+    expect(result).toEqual(mockData);
   });
 });
 
-describe("getTreeSVG", () => {
-  it("test sample", () => {
-    expect(1).toBe(1);
-  });
-
-  it("test sample2", () => {
-    expect(1).toBe(1);
+describe.skip("getTreeSVG", () => {
+  it("null을 입력하면 null을 반환합니다.", () => {
+    expect(getTreeSVG(null)).toBe(null);
   });
 });
